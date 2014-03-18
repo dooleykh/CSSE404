6cc88bf1e261773e58e4e13c6089cacb379b9374
Kyle Dooley <dooleykh@rose-hulman.edu>
2014-03-18 18:07:25 -0400

    Made changes to Milestone readme and recommitted an invalid test file.

---
+++ testcases/some-valid.java
@@ -0,0 +1,20 @@
+classname Classname someclass class1 variable2
+invalid-id also_invalid
+
+0 1 100231 101
+00 001 0001030 020110
+
+class public static extends void int boolean if else while return null true false this new String main System.out.println
+
++ - * / < <= >= > == != && || !
+; . , = ( ) { } [ ]
+
+# $ % ^ & _ - ~ `
+
+// {comment}
+
+/* also comment */
+
+notAComment
+
+/* invalid comment
