b3463f4134b39d1b90defbb3d699c525f06dfe7f
isabel <dillonscroggin@gmail.com>
2014-03-18 13:49:18 -0400

    another test case

---
+++ testcases/only-lexes.java
@@ -0,0 +1,17 @@
+class abstract abstract class extends class main extends main{int[] args}{
+	int int int main <= 42[];
+	main extends int{
+		system.out.println(class abstract){
+			if(return){
+				return;
+			}
+		}
+		;;;
+	}
+	/* nested comments don't exist /* */
+}
+
+main class extends abstract main void int main(int return false){
+	hello;
+}
+	
