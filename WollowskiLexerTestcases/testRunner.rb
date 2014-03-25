8d590d2a18bed0eb73f9d58d2e19d6d374866ef0
Kyle Dooley <dooleykh@rose-hulman.edu>
2014-03-25 14:55:05 -0400

    Pushed additional changes to testRunner.rb documentation

--- WollowskiLexerTestcases/testRunner.rb
+++ WollowskiLexerTestcases/testRunner.rb
@@ -1,8 +1,13 @@
+#This test harness runs the lexer on each test file and compares
+#the output to the expected output. If there's a difference, the test
+#and line number of the first error are displayed.
+
+#Run by setting /WollowskiLexerTestcases as your current directory
+#and runnning "ruby testRunner.rb" with Ruby 2.0 or higher.
 java = Dir["./FullTests/*.java"].sort
 output = Dir["./ExpectedOutput/*.out"].sort
 
 (0..java.length - 1).each do |i| 
-  #puts "Testing: #{java[i]}"
   `ruby ../lexer/lexer.rb #{java[i]} > out.txt`
   out = `cmp #{output[i]} out.txt`
   puts "#{java[i]}: #{out}" if out != ""
