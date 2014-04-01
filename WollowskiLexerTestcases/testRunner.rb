d436bf9edd721fe68c76d3959d662e0c9d58b1a4
Kyle Dooley <dooleykh@rose-hulman.edu>
2014-04-01 19:52:02 -0400

    Converted lexer into ruby module. Also changed functionality: the lexer returns a generator which returns indivdual words. This matches the recursive descent parser more closely and will minimize effort in integrating with the parser.

--- WollowskiLexerTestcases/testRunner.rb
+++ WollowskiLexerTestcases/testRunner.rb
@@ -7,7 +7,7 @@
 java = Dir["./FullTests/*.java"].sort
 output = Dir["./ExpectedOutput/*.out"].sort
 
-(0..java.length - 1).each do |i| 
+(0..java.length - 1).each do |i|
   `ruby ../lexer/lexer.rb #{java[i]} > out.txt`
   out = `cmp #{output[i]} out.txt`
   puts "#{java[i]}: #{out}" if out != ""
--- lexer/lexer.rb
+++ lexer/lexer.rb
@@ -1,67 +1,89 @@
-input = ARGF.readlines.map(&:lstrip).map(&:rstrip).map(&:chomp)
-token_classes = {
-  "ReservedWord" => /^(class|public|static|extends|void|int|boolean|if|else|while|return|null|true|false|this|new|String|main|System\.out\.println)(?=(\+|-|\*|\/|<=|<|>=|>|==|!=|&&|\|\||!|;|\.|,|=|\(|\)|\{|\}|\[|\])|\z|\s)(.*)$/,
-  "Operator" => /^(\+|-|\*|\/|<=|<|>=|>|==|!=|&&|\|\||!)(.*)$/,
-  "Delimiter" => /^(;|\.|,|=|\(|\)|\{|\}|\[|\])(.*)$/,
-  "Integer" => /^(0|[1-9][0-9]*)(.*)$/,
-  "ID" => /^([a-zA-Z])([a-zA-Z0-9]*)(.*)$/
-}
+module Lexer
 
-blockComment = false
-input.each { |line|
-  if blockComment or line["//"] or line["/*"]
-    l = ""
-    i = 0
-    until i > line.length - 1
-      if blockComment
-        if line[i, 2] == "*/"
-          blockComment = false
-          l += " "
-          i += 2
+  Word = Struct.new(:token, :value)
+
+  Token_classes =
+    {
+    :ReservedWord => /^(class|public|static|extends|void|int|boolean|if|else|while|return|null|true|false|this|new|String|main|System\.out\.println)(?=(\+|-|\*|\/|<=|<|>=|>|==|!=|&&|\|\||!|;|\.|,|=|\(|\)|\{|\}|\[|\])|\z|\s)(.*)$/,
+    :Operator => /^(\+|-|\*|\/|<=|<|>=|>|==|!=|&&|\|\||!)(.*)$/,
+    :Delimiter => /^(;|\.|,|=|\(|\)|\{|\}|\[|\])(.*)$/,
+    :Integer => /^(0|[1-9][0-9]*)(.*)$/,
+    :ID => /^([a-zA-Z])([a-zA-Z0-9]*)(.*)$/
+  }
+  
+  def Lexer.get_words(file)
+    input = File.open(file) do |f|
+      f.readlines.map(&:lstrip).map(&:rstrip).map(&:chomp)
+    end
+    blockComment = false
+    block_comment_line_num = -1
+    
+    words = Enumerator.new do |enum| 
+      input.each_with_index { |line, line_num|
+        if blockComment or line["//"] or line["/*"]
+          l = ""
+          i = 0
+          until i > line.length - 1
+            if blockComment
+              if line[i, 2] == "*/"
+                blockComment = false
+                l += " "
+                i += 2
+              else
+                i += 1
+              end
+            elsif line[i, 2] == "//"
+              break
+            elsif line[i, 2] == "/*"
+              blockComment = true
+              i += 2
+            else
+              l += line[i]
+              i += 1
+            end
+          end
         else
-          i += 1
+          l = line
         end
-      elsif line[i, 2] == "//"
-        break
-      elsif line[i, 2] == "/*"
-        blockComment = true
-        i += 2
-      else
-        l += line[i]
-        i += 1
+        l = l.strip
+
+        until l.empty?
+          l2 = l
+          
+          Token_classes.each_key{ |x|
+            if l =~ Token_classes[x]
+              if x == :ID
+                #We've matched an ID and may have to merge two pieces (if ID is longer than 1 char)
+                enum << Word.new(x, $1.strip + $2.strip)
+                l = $3.strip
+              elsif x == :ReservedWord
+                enum << Word.new(x, $1.strip)
+                l = $3.strip #Use $3 due to Regex Lookahead
+              else
+                enum << Word.new(x, $1.strip)
+                l = $2.strip
+              end
+              break
+            end
+          }
+          if l == l2
+            #No symbols matched. Consume one character and continue tokenizing (potential errors)
+            l = l[1..-1].strip
+            #We could add an error to the stream
+            #enum << Word.new(:UnknownToken, line_num + 1)
+          end
+        end
+      }
+      if blockComment
+        #Hanging block comment, so for ease raise an error
+        enum << Word.new(:BlockCommentError, block_comment_line_num + 1)
       end
     end
-  else
-    l = line
+    words
   end
-  l.lstrip
   
-  until l.empty?
-    l2 = l
-    token_classes.each_key{ |x|
-      if l =~ token_classes[x]
-        if x == "ID"
-          #We've matched an ID and may have to merge two pieces (if ID is longer than 1 char)
-          puts "#{x}, #{$1 + $2}"
-          l = $3.lstrip
-        elsif x == "ReservedWord"
-          puts "#{x}, #{$1}"
-          l = $3.lstrip #Use $3 due to Regex Lookahead
-        else
-          puts "#{x}, #{$1}"
-          l = $2.lstrip
-        end
-        break
-      end     
-    }
-    if l == l2
-      #No symbols matched. Consume one character and continue tokenizing (potential errors)
-      #TODO: Decide on approach for failures in tokenizing
-      l = l[1..-1]
-    end
+  if __FILE__ == $PROGRAM_NAME
+    words = get_words(File.absolute_path(ARGF.filename))
+    words.each { |word| puts "#{word.token}, #{word.value}"}
   end
-
-  if blockComment
-    #TODO: If reached, block comment was not closed. Decide how to handle. Halt? Parse? Alert developer?
-  end
-}
+end
