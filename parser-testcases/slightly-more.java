a61443f4ae8c55818049a0d4164c278b6a37d6ef
isabel <dillonscroggin@gmail.com>
2014-04-07 17:38:05 -0400

    More valid testcases

---
+++ parser-testcases/slightly-more.java
@@ -0,0 +1,6 @@
+class mainclass{
+	public static void main(String[] args){
+		int j = 4;
+		int k = j+2;
+	}
+}
---
+++ parser-testcases/with-other-class.java
@@ -0,0 +1,12 @@
+class mainclass{
+	public static void main(String[] args){
+		int j = 4;
+		int k = j+2;
+	}
+}
+
+class otherClass{
+	public int someMethod(int k){
+		return k * k;
+	}
+}
