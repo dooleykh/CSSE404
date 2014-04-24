c1ff2344329f45790cd9e47ffb2dae89d36373ea
isabel <dillonscroggin@gmail.com>
2014-04-24 01:54:47 -0400

    Type checker is all there, not tested yet

--- typeChecker/typeCheck.rb
+++ typeChecker/typeCheck.rb
@@ -1,313 +1,73 @@
 require_relative "../parser/parser.rb"
 require_relative  "../lexer/lexer.rb"
 
-#def getType(tree)
-#  :int
-#end
+GlobalEnv = Hash.new
+failed = false
 
-#
-#def lookup(symbol, env)
-#  if env.nil?
-#    return nil
-#  end
-#
-#  if symbol == "this"
-#  end
-#  if env.last[symbol]
-#    return env.last[symbol]
-#	end
-#
-#	if env.length > 2
-#		a = lookup(symbol, env[0..-2])
-#		return a if a
-#	end
-#
-#	if env.last[:super]
-#    a = lookup(symbol, [env.first, lookup(env.last[:super], [env.first])])
-#    return a if a
-#	end
-#
-#	if env.first[symbol]
-#		return env.first[symbol]
-#	end
-#
-#	return nil
-#end
-#
-#
-##turns a type token into int, boolean, or a name
-#def resolveType(token)
-#	if token.type == :int or token.type == :boolean
-#		token = token.type
-#	else
-#		token = token.value
-#	end
-#	token
-#end
-#
-##returns the type given by performing math on two types
-#def mathTypes(typeA, typeB)
-#	if typeA == :int
-#		if typeB == :int or typeB == :boolean
-#			return :int
-#		else
-#			raise JavaSyntaxError
-#		end
-#	elsif typeA == :boolean
-#		if typeB == :boolean
-#			return :boolean
-#		elsif typeB == :int
-#			return :int
-#		else
-#			raise JavaSyntaxError
-#		end
-#	else
-#		raise JavaSyntaxError
-#	end
-#end
-#
-#def matchArgs(method, exprls, env)
-#	return true
-#end
-#
-#def expr8chain(symbol, type, env)
-#  method = lookup(symbol.children[0].value, env)
-#  if method.nil?
-#		puts "Method #{symbol.children[0].value} doesn't exist"
-#		raise JavaSyntaxError
-#	end
-#
-#	unless matchArgs(method, symbol.children[1], env)
-#		puts "Args don't match"
-#		raise JavaSyntaxError
-#	end
-#
-#	type = method.returnValue
-#	
-#	if symbol.children.last.name == :Expr8Pr
-#		return expr8chain(symbol.children.last, type, env)
-#	else
-#		return type
-#	end
-#end
-#
-#
-#def resolveExprType(tree, env)
-#	case tree.name
-#	when :Expr, :Expr2, :Expr3, :Expr4, :Expr5, :Expr6
-#		if tree.children.length == 1
-#			return resolveExprType(tree.children[0], env)
-#		end
-#		a = resolveExprType(tree.children[0], env)
-#		b = resolveExprType(tree.children[1], env)
-#		return mathTypes(a, b)
-#
-#	when :Expr7
-#		a = resolveExprType(tree.children[0], env)
-#		if tree.type == '!'
-#			if a == :int or a == :boolean
-#				return :boolean
-#			else
-#				raise JavaSyntaxError
-#			end
-#		elsif tree.type = '-'
-#			if a == :int or a == :boolean
-#				return a
-#			end
-#		else
-#			return a
-#		end
-#
-#	when :Expr8
-#		if tree.children.length == 1
-#			return resolveExprType(tree.children[0], env)
-#		end
-#
-#		a = resolveExprType(tree.children[0], env)
-#		return expr8chain(tree.children[1], a, env)
-#
-#	when :Expr9
-#		item = tree.children[0]
-#		case tree.type
-#		when :new
-#			return item.children[0].value
-#		when :id
-#      l = lookup(item.value, env)
-#      if l.nil?
-#        #variable doesn't exist
-#        raise JavaSyntaxError
-#      end
-#      return l.type
-#		when :this
-#      return lookup(:this, env)
-#		when :integer, :null
-#			return :int
-#		when :true, :false
-#			return :boolean
-#		when :expr
-#			return resolveExprType(tree.children[0], env)
-#		end
-#  end
-#  
-#end
-#
-#def walk(tree, tables)
-#  if not tree
-#    return
-#  end
-#  case tree.name
-#  when :Program
-#    env = [{}]
-#    tree.children.each { |c| walk(c, env) }
-#  when :ClassDeclSt
-#    tree.children.each { |c| walk(c, tables) }
-#  when :MainClassDecl
-#    tables << {}
-#    #ignoring String[] ID since it can't be referenced...ever
-#    walk(tree.children[2], tables)
-#    t = tables.pop
-#    tables.last[tree.children[0].value] = t
-#  when :StmtSt
-#    tree.children.each { |c| walk(c, tables) }
-#  when :ClassDecl
-#    if tree.children.length > 1 and tree.children[1].type == :id
-#      #extending class
-#      if not tables.last[tree.children[1].value]
-#        puts "ERROR"
-#        raise JavaSyntaxError
-#      end
-#      tables << {}
-#      tables.last[:super] = tree.children[1].value
-#    else
-#      tables << {}
-#    end
-#        tables.last[:this] = tables.last
-#    tree.children.each { |c|
-#      unless c.name == :id
-#        walk(c, tables)
-#      end }
-#    env = tables.pop
-#    tables.last[tree.children[0].value] = env
-#  when :MethodDeclSt
-#    tree.children.each { |c| walk(c, tables) }
-#  when :ClassVarDeclSt
-#    tree.children.each { |c| walk(c, tables) }
-#  when :MethodDecl
-#    if tables.last[tree.children[1].value]
-#      #method exists. Throw
-#      raise JavaSyntaxError
-#    end
-#    if tables.last[:super]
-#      l = lookup(tree.children[1], tables) 
-#      if l
-#        unless l.is_a?(JavaMethod) && l.type == resolveType(tree.children[0])
-#          puts "HELP ERROR"
-#          raise JavaSyntaxError
-#        end
-#      end
-#    end
-#    formal = tree.children[2]
-#    if formal.name == :FormalSt
-#      while true
-#        case formal.name
-#        when :FormalSt
-#          if formal.children.size > 0
-#            formal = formal.children[0]
-#          else
-#            break
-#          end
-#        when :FormalPl
-#          method.arguements.push resolveType(formal.children[0].children[0])
-#          formal = formal.children[1]
-#        when :Formal
-#          method.arguements.push resolveType(formal.children[0].children[0])
-#        end
-#      end
-#    end
-#
-#    ret = resolveType(tree.children[0])
-#    method = JavaMethod.new
-#    method.returnValue = ret
-#
-#    tables.last[tree.children[1].value] = method
-#    unless ret == resolveExprType(tree.children[-1], tables)
-#      # p ret
-#      # p resolveExprType(tree.children[-1], tables)
-#      # puts tree.children[-1].name
-#      puts "MethodDecl ERROR"
-#      raise JavaSyntaxError
-#    end
-#
-#    tables << {}
-#    tree.children[1..-1].each { |c| walk(c, tables) }
-#
-#
-#    tables.pop
-#
-#  when :ClassVarDecl
-#    unless lookup(tree.children[1].value, tables).nil?
-#		  puts "ERROR - variable #{tree.children[0]} already defined"
-#      raise JavaSyntaxError
-#	  end
-#
-#	  tables.last[tree.children[1].value] = Variable.new(resolveType(tree.children[0]))
-#  when :FormalSt
-#    tree.children.each { |c| walk(c, tables) }
-#
-#  when :FormalPl
-#	  tree.children.each { |c| walk(c, tables) }
-#    
-#  when :Formal
-#	  tables.last[tree.children[0].value] = Variable.new(resolveType(tree.children[1]))
-#
-#  when :Type
-#	  #We shouldn't ever get here
-#	  puts "ERROR - I don't even know"
-#		raise JavaSyntaxError
-#    
-#  when :Stmt
-#	  case tree.type
-#	  when :block
-#		  tables << {}
-#		  tree.children.each { |c| walk(c, tables) }
-#		  tables.pop
-#      
-#	  when :if, :while
-#        tree.children.each { |c| walk(c, tables) }
-#
-#	  when :var_dec
-#		  unless resolveType(tree.children[0]) == resolveExprType(tree.children[2], tables)
-#			  puts "Error - that expression doesn't make that type"
-#        raise JavaSyntaxError
-#		  end
-#
-#		  if tables.last[tree.children[1].value]
-#			  puts "ERROR - variable #{tree.children[1]} already defined"
-#        raise JavaSyntaxError
-#		  end
-#
-#		  var = Variable.new(resolveType(tree.children[0]))
-#
-#		  tables.last[tree.children[1].value] = var
-#
-#	  when :var_asgn
-#
-#		  unless lookup(tree.children[0].value, env).type == resolveTypeExpr(tree.children[1])
-#			  puts "Error - that expression doesn't make that type"
-#        raise JavaSyntaxError
-#		  end
-#
-#		  unless tables.last[tree.children[0].value]
-#			  puts "ERROR - variable #{tree.children[0]} not already defined"
-#        raise JavaSyntaxError
-#		  end
-#
-#	  when :print
-#		  resolveExprType(tree.children[0], tables)
-#    end
-#    
-#  end
-#end
+def walk(tree, env)
+	if not tree
+		return
+	end
+
+	case tree.name
+	when :Program, :ClassDeclSt, :ClassDecl, :MethodDeclSt, :ClassVarDeclSt, :MainClassDecl, :MethodDecl, :ClassVarDecl, :FormalSt, :FormalPl, :Formal, :Type
+		puts "Shouldn't happen" #handled by earlier passes
+		failed = true
+
+	when :StmtSt
+		tree.children.each { |c| walk(c, env) }
+
+	when :Expr
+		resolveExprType(tree, env)
+
+	when :Stmt
+	  case tree.type
+	  when :block
+		  env << {}
+		  tree.children.each { |c| walk(c, env) }
+		  env.pop
+      
+	  when :if, :while
+        tree.children.each { |c| walk(c, env) }
+
+	  when :var_dec
+		  unless resolveType(tree.children[0]) == resolveExprType(tree.children[2], env)
+			  puts "Error - that expression doesn't make that type"
+			  failed = true
+
+        raise JavaSyntaxError
+		  end
+
+		  if env.last[tree.children[1].value]
+			  puts "ERROR - variable #{tree.children[1]} already defined"
+			  failed = true
+        raise JavaSyntaxError
+		  end
+
+		  var = JavaVariable.new(resolveType(tree.children[0]))
+
+		  env.last[tree.children[1].value] = var
+
+	  when :var_asgn
+
+		  unless lookup(tree.children[0].value, env).type == resolveTypeExpr(tree.children[1])
+			  puts "Error - that expression doesn't make that type"
+			  failed = true
+        raise JavaSyntaxError
+		  end
+
+		  unless env.last[tree.children[0].value]
+			  puts "ERROR - variable #{tree.children[0]} not already defined"
+			  failed = true
+        raise JavaSyntaxError
+		  end
+
+	  when :print
+		  resolveExprType(tree.children[0], env)
+    end
+    
+  end
+end
 
 # turns a :type into :int, :boolean, or the string of the id
 def resolveType(token)
@@ -349,20 +109,21 @@ class JavaClass
 	@name
 	@env
 	@parseTree
+	@main
 
-	attr_accessor :env, :name, :parseTree
+	attr_accessor :env, :name, :parseTree, :main
 
 	def initialize()
 		@env = Hash.new
+		@main = false
 	end
 end
 
 class JavaSyntaxError < Exception
 end
 
-GlobalEnv = Hash.new
 
-def walk(parse_tree)
+def passes(parse_tree)
 
 	#first pass - class prototypes
 	classes = Array.new
@@ -380,6 +141,7 @@ def walk(parse_tree)
 
 		if GlobalEnv[name]
 			puts "Error - class #{name} already declared"
+			  failed = true
 			next
 		end
 
@@ -395,13 +157,15 @@ def walk(parse_tree)
 		#For each class
 
 		classObject.name = classObject.parseTree.children[0].value
+		classObject.env[:this] = classObject.name
 
 		if classObject.parseTree.name == :MainClassDecl
 			classObject.env['main'] = JavaMethod.new()
 			classObject.env['main'].args = [:StringArr]
 			classObject.env['main'].argnames = [ classObject.parseTree.children[1].value ]
 			classObject.env['main'].name = 'main'
-			classObject.env['main'].parseTree = classObject.parseTree.children[2]
+			classObject.env['main'].parseTree = classObject.parseTree
+			classObject.main = true
 			# We don't really have to worry about this very much because you
 			# can't ever actually call main because you can't pass it a string
 			# array
@@ -445,6 +209,7 @@ def walk(parse_tree)
 				name = varTree.children[1].value
 				if classObject.env[name]
 					puts "Error - class variable #{name} already declared"
+			  failed = true
 					next
 				end
 
@@ -458,6 +223,7 @@ def walk(parse_tree)
 
 				if classObject.env[name]
 					puts "Error - method #{name} already declared"
+			  failed = true
 					next
 				end
 
@@ -511,14 +277,230 @@ def walk(parse_tree)
 		classObject.parseTree = nil
 	}
 
+	# Third pass - validate methods
+	GlobalEnv.each_value { |i|
+
+		# Mainclass (and therefore main method)
+		if i.main
+			method = i.env['main']
+
+			if method.parseTree.children.size > 2
+				env = [method.env, Hash.new]
+				# No point in binding String[] args because it can't be referenced
+
+				walk(method.parseTree.children[2], env)
+			end
+			method.parseTree = nil
+			next
+		end
+
+		# Each method in each class
+		i.env.each_value { |j|
+			unless j.class == JavaMethod
+				next
+			end
+
+			# Bind the arguements in the new environment
+			env = [i.env, Hash.new]
+			j.args.each_index { |k|
+				var = JavaVariable.new(j.args[k])
+				var.name = j.argnames[k]
+				env.last[var.name] = var
+			}
+
+			#find the method body
+			stmt = nil
+			if j.parseTree.children.size > 2
+				if j.parseTree.children[1].name == :StmtSt
+					stmt = j.parseTree.children[1]
+				elsif j.parseTree.children[2].name == :StmtSt
+					stmt = j.parseTree.children[2]
+				end
+			end
+			
+			#validate the method body
+			if stmt
+				walk(stmt, env)
+			end
+
+			#verify the return type
+			unless resolveExprType(j.parseTree.children[-1], env) == j.ret
+				puts "Error - method #{j.name} does not return the correct type"
+			  failed = true
+			end
+
+			j.parseTree = nil
+		}
+	}
+
+
 end
 
+def lookup(symbol, env)
+	if env == nil
+		return GlobalEnv[symbol]
+	end
 
+
+	if env.last[symbol]
+		return env.last[symbol]
+	elsif env.size > 1
+		return lookup(symbol, env[0..-2])
+	elsif env.first[:super]
+		return lookup(symbol, [env.first[:super].env])
+	else
+		return lookup(symbol, nil)
+	end
+end
+
+#returns the type given by performing math on two types
+def mathTypes(typeA, typeB)
+	if typeA == :int
+		if typeB == :int or typeB == :boolean
+			return :int
+		else
+			raise JavaSyntaxError
+		end
+	elsif typeA == :boolean
+		if typeB == :boolean
+			return :boolean
+		elsif typeB == :int
+			return :int
+		else
+			raise JavaSyntaxError
+		end
+	else
+		raise JavaSyntaxError
+	end
+end
+
+def resolveExprType(tree, env)
+	case tree.name
+	when :Expr, :Expr2, :Expr3, :Expr4, :Expr5, :Expr6
+		if tree.children.length == 1
+			return resolveExprType(tree.children[0], env)
+		end
+		a = resolveExprType(tree.children[0], env)
+		b = resolveExprType(tree.children[1], env)
+		return mathTypes(a, b)
+
+	when :Expr7
+		a = resolveExprType(tree.children[0], env)
+		if tree.type == '!'
+			if a == :int or a == :boolean
+				return :boolean
+			else
+				raise JavaSyntaxError
+			end
+		elsif tree.type = '-'
+			if a == :int or a == :boolean
+				return a
+			end
+		else
+			return a
+		end
+
+	when :Expr8
+		if tree.children.length == 1
+			return resolveExprType(tree.children[0], env)
+		end
+
+		a = resolveExprType(tree.children[0], env)
+		return expr8chain(tree.children[1], a, env)
+
+	when :Expr9
+		item = tree.children[0]
+		case tree.type
+		when :new
+			return item.children[0].value
+		when :id
+      l = lookup(item.value, env)
+      if l.nil?
+        #variable doesn't exist
+        raise JavaSyntaxError
+      end
+      return l.type
+		when :this
+      return lookup(:this, env)
+		when :integer, :null
+			return :int
+		when :true, :false
+			return :boolean
+		when :expr
+			return resolveExprType(tree.children[0], env)
+		end
+  end
+  
+end
+
+def matchArgs(method, argslist, env)
+	if argslist == nil
+		if method.args.size == 0
+			return true
+		else
+			return false
+		end
+	end
+
+	types = Array.new
+	while true
+		case argslist.name
+		when :Expr8St
+			argslist = argslist.children[0]
+		when :Expr8Pl
+			types.push resolveExprType(argslist.children[0], env)
+			if(argslist.children.size > 1)
+				argslist = argslist.children[1]
+			else
+				break
+			end
+		end
+	end
+
+	if types.size != method.args.size
+		puts "Wrong number of arguements"
+			  failed = true
+		return false
+	end
+
+	types.each_index { |i|
+		if types[i] != method.args[i]
+			return false
+		end
+	}
+
+	return true
+end
+
+def expr8chain(symbol, type, env)
+  method = lookup(symbol.children[0].value, env)
+  if method.nil?
+		puts "Method #{symbol.children[0].value} doesn't exist"
+			  failed = true
+		raise JavaSyntaxError
+	end
+
+	unless matchArgs(method, symbol.children[1], env)
+		puts "Args don't match"
+			  failed = true
+		raise JavaSyntaxError
+	end
+
+	type = method.ret
+	
+	if symbol.children.last.name == :Expr8Pr
+		return expr8chain(symbol.children.last, type, env)
+	else
+		return type
+	end
+end
 
 if __FILE__ == $PROGRAM_NAME
   source = File.absolute_path(ARGF.filename)
   parse_tree = program(Lexer.get_words(source))
-  walk(parse_tree)
-  p GlobalEnv
+  passes(parse_tree)
+  unless failed
+	  puts 'Yokay!'
+  end
 end
 
